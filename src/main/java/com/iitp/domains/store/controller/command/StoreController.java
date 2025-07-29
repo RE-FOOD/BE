d32a5396385213722cd4f6843d7cdb9d48a1fe7d
baejiwon <qowhxk@naver.com>
2025-07-29 14:10:59 +0900

    Controller 뼈대 생성 / CUD, Read 분리

---
+++ src/main/java/com/iitp/domains/store/controller/command/StoreController.java
@@ -0,0 +1,4 @@
+package com.iitp.domains.store.controller.command;
+
+public class StoreController {
+}
---
+++ src/main/java/com/iitp/domains/store/controller/query/GetStoreController.java
@@ -0,0 +1,4 @@
+package com.iitp.domains.store.controller.query;
+
+public class GetStoreController {
+}
