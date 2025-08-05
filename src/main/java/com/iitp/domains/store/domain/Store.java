34f2546d939eaad6c840fd7e9038a11b30f601c2
baejiwon <qowhxk@naver.com>
2025-08-05 10:52:04 +0900

    feat: swagger 기능 추가

--- src/main/java/com/iitp/domains/store/domain/Store.java
+++ src/main/java/com/iitp/domains/store/domain/Store.java
@@ -1,6 +1,19 @@
 package com.iitp.domains.store.domain;
 
 import com.iitp.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "store")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Store extends BaseEntity {
+    @Id
+    @Column(name = "store_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 }
