b2026d28c2a0fac0e27028ba49eaeb29a7964a08
baejiwon <qowhxk@naver.com>
2025-08-05 12:09:59 +0900

    feat: Store 패키지 이동

--- src/main/java/com/iitp/domains/store/repository/StoreRepository.java
+++ src/main/java/com/iitp/domains/store/repository/StoreRepository.java
@@ -1,6 +1,6 @@
 package com.iitp.domains.store.repository;
 
-import com.iitp.domains.store.domain.Store;
+import com.iitp.domains.store.domain.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Long>, StoreRepositoryCustom {
