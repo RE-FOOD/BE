f7ac5a5eb9f64808e117bbf8772ba25718bfeda2
baejiwon <qowhxk@naver.com>
2025-08-07 16:28:58 +0900

    feat: StoreList Query Result Mapper 생성

---
+++ src/main/java/com/iitp/domains/store/repository/mapper/StoreListQueryResult.java
@@ -0,0 +1,13 @@
+package com.iitp.domains.store.repository.mapper;
+
+import com.iitp.domains.store.domain.StoreStatus;
+
+import java.util.List;
+
+public record StoreListQueryResult(
+        Long id,
+        String name,
+        StoreStatus status,
+        String imageKey
+) {
+}
