a0836b5ad4a33153f73369686a49783fcd9bad8f
wonseok2877 <wonseok2877@gmail.com>
2025-08-10 16:06:48 +0900

    feat: 찜 Repository 찜 조회

---
+++ src/main/java/com/iitp/domains/favorite/repository/FavoriteRepository.java
@@ -0,0 +1,11 @@
+package com.iitp.domains.favorite.repository;
+
+import com.iitp.domains.favorite.domain.entity.Favorite;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+    Optional<Favorite> findByMemberIdAndStoreId(Long memberId, Long storeId);
+}
