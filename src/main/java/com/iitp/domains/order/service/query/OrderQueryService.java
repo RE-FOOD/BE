e8e5062d711b2d499cdf28b00d633225345cf5c5
wonseok2877 <wonseok2877@gmail.com>
2025-08-23 21:26:54 +0900

    docs: 주문-장바구니-메뉴 리스트 조회 메서드 고민

--- src/main/java/com/iitp/domains/order/service/query/OrderQueryService.java
+++ src/main/java/com/iitp/domains/order/service/query/OrderQueryService.java
@@ -2,8 +2,10 @@ package com.iitp.domains.order.service.query;
 
 import com.iitp.domains.order.domain.entity.Order;
 import com.iitp.domains.order.repository.OrderRepository;
+import com.iitp.domains.store.domain.entity.Menu;
 import com.iitp.global.exception.ExceptionMessage;
 import com.iitp.global.exception.NotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,5 +23,14 @@ public class OrderQueryService {
             throw new NotFoundException(ExceptionMessage.ORDER_NOT_FOUND);
         }
         return order;
+    }
+
+
+    public List<Menu> findMenuList(Long orderId) {
+        // Order -> Cart -> CartMenu -> Menu 리스트 조회
+        // 1. 객체적으로 접근하기. 근데 지금은 CartMenu에서 Menu 엔티티 객체가 아닌 ID만을 FK로 갖고 있음.
+        // 2. 쿼리를 통해 접근하기. QueryDSL을 써야 할듯? SELECT * FROM MENU WHERE
+
+        return null;
     }
 }
