f9f57c61108301c2eee57b50dcdc6341337a0397
wonseok2877 <wonseok2877@gmail.com>
2025-08-27 15:38:31 +0900

    fix: 리뷰 조회 NPE 오류

--- src/main/java/com/iitp/domains/review/service/query/ReviewQueryService.java
+++ src/main/java/com/iitp/domains/review/service/query/ReviewQueryService.java
@@ -1,12 +1,16 @@
 package com.iitp.domains.review.service.query;
 
+import com.iitp.domains.cart.domain.entity.CartMenu;
+import com.iitp.domains.cart.service.query.CartQueryService;
 import com.iitp.domains.member.domain.entity.Member;
 import com.iitp.domains.member.service.query.MemberQueryService;
 import com.iitp.domains.review.domain.entity.Review;
+import com.iitp.domains.review.dto.response.MyReviewResponse;
 import com.iitp.domains.review.dto.response.ReviewResponse;
 import com.iitp.domains.review.repository.ReviewRepository;
 import com.iitp.domains.store.domain.entity.Menu;
 import com.iitp.domains.store.domain.entity.Store;
+import com.iitp.domains.store.repository.menu.MenuRepository;
 import com.iitp.domains.store.service.query.StoreQueryService;
 import com.iitp.global.common.response.TwoWayCursorListResponse;
 import java.util.List;
@@ -23,6 +27,7 @@ public class ReviewQueryService {
     private final ReviewRepository reviewRepository;
     private final MemberQueryService memberQueryService;
     private final StoreQueryService storeQueryService;
+    private final MenuRepository menuRepository;
 
     /**
      * API 응답 메서드
@@ -30,13 +35,22 @@ public class ReviewQueryService {
     public TwoWayCursorListResponse<ReviewResponse> readStoreReviews(Long storeId, Long cursorId, int limit) {
         Store store = storeQueryService.findExistingStore(storeId);
         List<ReviewResponse> result = getStoreReviews(store.getId(), cursorId, limit);
-        return new TwoWayCursorListResponse<>(result.getFirst().id(), result.getLast().id(), result);
+
+        return new TwoWayCursorListResponse<>(
+                result.isEmpty() ? null : result.getFirst().id(),
+                result.isEmpty() ? null : result.getLast().id(),
+                result
+        );
     }
 
-    public TwoWayCursorListResponse<ReviewResponse> readMyReviews(long memberId, long cursorId, int limit) {
+    public TwoWayCursorListResponse<MyReviewResponse> readMyReviews(long memberId, long cursorId, int limit) {
         Member author = memberQueryService.findMemberById(memberId);
-        List<ReviewResponse> result = getMyReviews(author.getId(), cursorId, limit);
-        return new TwoWayCursorListResponse<>(result.getFirst().id(), result.getLast().id(), result);
+        List<MyReviewResponse> result = getMyReviews(author.getId(), cursorId, limit);
+        return new TwoWayCursorListResponse<>(
+                result.isEmpty() ? null : result.getFirst().id(),
+                result.isEmpty() ? null : result.getLast().id(),
+                result
+        );
     }
 
     /**
@@ -47,24 +61,38 @@ public class ReviewQueryService {
         return convertToResponse(reviews);
     }
 
-    private List<ReviewResponse> getMyReviews(long authorId, Long cursorId, int limit) {
+    private List<MyReviewResponse> getMyReviews(long authorId, Long cursorId, int limit) {
         List<Review> reviews = reviewRepository.findReviewsByMember(authorId, cursorId, limit);
-        return convertToResponse(reviews);
+        return convertToMyResponse(reviews);
     }
 
     /**
      * 공통 메서드
      */
-    private static List<ReviewResponse> convertToResponse(List<Review> reviews) {
+    private List<ReviewResponse> convertToResponse(List<Review> reviews) {
         return reviews.stream()
                 .map(it -> ReviewResponse.from(it, getOrderedMenusOfReview(it)))
                 .toList();
     }
 
-    // TODO: 주문 구현 후 리뷰의 실제 주문-장바구니-메뉴 리스트 가져오는 쪽으로 수정
-    private static List<Menu> getOrderedMenusOfReview(Review review) {
+    private List<MyReviewResponse> convertToMyResponse(List<Review> reviews) {
+        return reviews.stream()
+                .map(it -> MyReviewResponse.from(it, getOrderedMenusOfReview(it)))
+                .toList();
+    }
+
+    private List<Menu> getOrderedMenusOfReview(Review review) {
         // 임시로 해당 가게의 모든 메뉴를 반환
-        return review.getStore().getMenus();
+        System.out.println("review.getOrder().getId() = " + review.getOrder().getId());
+        System.out.println("review.getOrder().getCart().getId() = " + review.getOrder().getCart().getId());
+
+        List<Long> menuIdList = review.getOrder().getCart().getCartMenus().stream()
+                .map(CartMenu::getMenuId)
+                .toList();
+
+        menuIdList.forEach(it-> System.out.println("it = " + it));
+
+        return menuRepository.findAllById(menuIdList);
     }
 
 }
