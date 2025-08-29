291f0b9dbc7378f674213e10c177adda8ccc4b8d
wonseok2877 <wonseok2877@gmail.com>
2025-08-29 13:47:27 +0900

    fix: 가게 상세 조회 캐싱 로직 제거

--- src/main/java/com/iitp/domains/store/service/query/StoreQueryService.java
+++ src/main/java/com/iitp/domains/store/service/query/StoreQueryService.java
@@ -61,15 +61,15 @@ public class StoreQueryService {
 
     public StoreDetailResponse findStoreData(Long memberId, Long storeId) {
         // 캐시된 데이터가 있고, 다른 가게를 요청하는 경우
-        if (cacheService.hasCachedData() && !storeId.equals(currentCachedStoreId)) {
-            cacheService.clearCache();
-        }
-
-        // 캐시에서 데이터 조회
-        StoreDetailResponse cachedData = cacheService.getCachedStoreDetail();
-        if (cachedData != null && storeId.equals(currentCachedStoreId)) {
-            return cachedData;
-        }
+//        if (cacheService.hasCachedData() && !storeId.equals(currentCachedStoreId)) {
+//            cacheService.clearCache();
+//        }
+//
+//        // 캐시에서 데이터 조회
+//        StoreDetailResponse cachedData = cacheService.getCachedStoreDetail();
+//        if (cachedData != null && storeId.equals(currentCachedStoreId)) {
+//            return cachedData;
+//        }
 
         Store store = findExistingStore(storeId);
 
@@ -90,8 +90,8 @@ public class StoreQueryService {
                 .from(store, imageUrls, menus, isFavored, roundedAvg, reviewCount);
 
         // 캐시에 저장
-        cacheService.cacheStoreDetail(response);
-        currentCachedStoreId = storeId;
+//        cacheService.cacheStoreDetail(response);
+//        currentCachedStoreId = storeId;
         return response;
     }
 
@@ -111,6 +111,12 @@ public class StoreQueryService {
     public Store findExistingStore(Long storeId) {
         Store store = storeRepository.findByStoreId(storeId)
                 .orElseThrow(() -> new NotFoundException(ExceptionMessage.DATA_NOT_FOUND));
+        System.out.println("store.getId() = " + store.getId());
+        System.out.println("store.getName() = " + store.getName());
+        System.out.println("store.getFavorites() = " + store.getFavorites());
+        store.getFavorites().forEach(it-> System.out.println("it.getId() = " + it.getId()));
+
+
         if (store.getIsDeleted()) {
             throw new NotFoundException(ExceptionMessage.DATA_NOT_FOUND);
         }
