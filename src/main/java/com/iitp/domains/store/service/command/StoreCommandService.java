05f59ca0b647d11d5434f869a2b17443a2502056
baejiwon <qowhxk@naver.com>
2025-09-01 14:07:26 +0900

    fix : 가게 인사이트 매개변수 값 변경 memberId -> storeId 변경

--- src/main/java/com/iitp/domains/store/service/command/StoreCommandService.java
+++ src/main/java/com/iitp/domains/store/service/command/StoreCommandService.java
@@ -126,8 +126,8 @@ public class StoreCommandService {
 
 
     public InsightResponse findInsight(Long memberId) {
-
-        InsightResponse response = orderQueryService.findInsight(memberId);
+        Store store = validateStoreExistsFromMemberId(memberId);
+        InsightResponse response = orderQueryService.findInsight(store.getId());
         return response;
     }
 
