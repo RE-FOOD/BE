3dde245625adf5db7c50b00721453bca590edf5f
baejiwon <qowhxk@naver.com>
2025-09-29 21:41:10 +0900

    refactor : refactor

--- src/main/java/com/iitp/global/config/AsyncConfig.java
+++ src/main/java/com/iitp/global/config/AsyncConfig.java
@@ -1,10 +1,11 @@
 package com.iitp.global.config;
 
-import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
@@ -13,8 +14,8 @@ public class AsyncConfig {
     @Bean(name = "AsyncBean")
     public Executor asyncThreadTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(50); // 기본 스레드 수
-        executor.setMaxPoolSize(100); // 최대 스레드 수
+        executor.setCorePoolSize(50);
+        executor.setMaxPoolSize(100);
         executor.setQueueCapacity(200); // Queue 사이즈
         executor.setThreadNamePrefix("AsyncThread");
         return executor;
